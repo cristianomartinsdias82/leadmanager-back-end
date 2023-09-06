e04108c7abb77a2a4498cacb1e58034e06de488e
cristianomartinsdias82 <crisdi@gmail.com>
2023-09-06 17:46:30 -0300

    Code cleanup (removed StronglyTypedId and Microsoft.Logging libraries references from Core project)

--- src/libs/Core/DomainEvents/LeadRemoved/LeadRemovedDomainEventHandler.cs
+++ src/libs/Core/DomainEvents/LeadRemoved/LeadRemovedDomainEventHandler.cs
@@ -1,24 +1,17 @@
 ﻿using MediatR;
-using Microsoft.Extensions.Logging;
 using Shared.Events.DomainEvents;
 
 namespace Core.DomainEvents.LeadRemoved;
 
 internal sealed class LeadRemovedDomainEventHandler : ApplicationDomainEventHandler<LeadRemovedDomainEvent>
 {
-    private readonly ILogger<LeadRemovedDomainEventHandler> _logger;
-
     public LeadRemovedDomainEventHandler(
-        IMediator mediator,
-        ILogger<LeadRemovedDomainEventHandler> logger) : base(mediator)
+        IMediator mediator) : base(mediator)
     {
-        _logger = logger;
     }
 
     public override async Task Handle(LeadRemovedDomainEvent notification, CancellationToken cancellationToken)
     {
-        _logger.LogInformation("A lead exclusion domain event has happened: {event} - Data: {notification}", notification.GetType().Name, notification.Lead);
-
         await Task.CompletedTask;
     }
 }
--- src/libs/Core/DomainEvents/LeadUpdated/LeadUpdatedDomainEventHandler.cs
+++ src/libs/Core/DomainEvents/LeadUpdated/LeadUpdatedDomainEventHandler.cs
@@ -1,24 +1,17 @@
 ﻿using MediatR;
-using Microsoft.Extensions.Logging;
 using Shared.Events.DomainEvents;
 
 namespace Core.DomainEvents.LeadUpdated;
 
 internal sealed class LeadUpdatedDomainEventHandler : ApplicationDomainEventHandler<LeadUpdatedDomainEvent>
 {
-    private readonly ILogger<LeadUpdatedDomainEventHandler> _logger;
-
     public LeadUpdatedDomainEventHandler(
-        IMediator mediator,
-        ILogger<LeadUpdatedDomainEventHandler> logger) : base(mediator)
+        IMediator mediator) : base(mediator)
     {
-        _logger = logger;
     }
 
     public override async Task Handle(LeadUpdatedDomainEvent notification, CancellationToken cancellationToken)
     {
-        _logger.LogInformation("A lead data update domain event has happened: {event} - Data: {notification}", notification.GetType().Name, notification.Lead);
-
         await Task.CompletedTask;
     }
 }
