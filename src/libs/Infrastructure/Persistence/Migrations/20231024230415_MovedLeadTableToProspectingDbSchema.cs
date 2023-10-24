e2499a496ce8a88887223a3d3807377eaebc4d43
cristianomartinsdias82 <crisdi@gmail.com>
2023-10-24 20:15:15 -0300

    Move leads table to Prospecting database schema

---
+++ src/libs/Infrastructure/Persistence/Migrations/20231024230415_MovedLeadTableToProspectingDbSchema.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class MovedLeadTableToProspectingDbSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Prospecting");
+
+            migrationBuilder.RenameTable(
+                name: "Leads",
+                newName: "Leads",
+                newSchema: "Prospecting");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Leads",
+                schema: "Prospecting",
+                newName: "Leads");
+        }
+    }
+}
