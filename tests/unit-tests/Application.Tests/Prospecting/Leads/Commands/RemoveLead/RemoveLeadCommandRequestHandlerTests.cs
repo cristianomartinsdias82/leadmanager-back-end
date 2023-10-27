f03f8764dca6b28433cd88180e6e74c10af584b7
cristianomartinsdias82 <crisdi@gmail.com>
2023-10-27 18:58:22 -0300

    Implement unit tests for update and delete leads concurrency scenarios

--- tests/unit-tests/Application.Tests/Prospecting/Leads/Commands/RemoveLead/RemoveLeadCommandRequestHandlerTests.cs
+++ tests/unit-tests/Application.Tests/Prospecting/Leads/Commands/RemoveLead/RemoveLeadCommandRequestHandlerTests.cs
@@ -3,6 +3,7 @@ using Application.Prospecting.Leads.Commands.RemoveLead;
 using Domain.Prospecting.Entities;
 using FluentAssertions;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using NSubstitute;
 using Shared.Events;
 using Shared.Events.EventDispatching;
@@ -15,8 +16,8 @@ namespace Application.Tests.Prospecting.Leads.Commands.RemoveLead;
 public sealed class RemoveLeadCommandRequestHandlerTests
 {
     private readonly RemoveLeadCommandRequestHandler _handler;
-    private readonly ILeadRepository _leadRepository;
     private readonly IMediator _mediator;
+    private readonly ILeadRepository _leadRepository;
     private readonly IEventDispatching _eventDispatcher;
     private readonly CancellationTokenSource _cts;
 
@@ -27,6 +28,40 @@ public sealed class RemoveLeadCommandRequestHandlerTests
         _eventDispatcher = Substitute.For<IEventDispatching>();
         _handler = new RemoveLeadCommandRequestHandler(_mediator, _leadRepository, _eventDispatcher);
         _cts = new();
+    }
+
+    [Fact]
+    public async Task Handle_ConcurrentDeleteRequestsOverSameRecord_ShouldSignalConcurrencyIssue()
+    {
+        //Arrange
+        var lead = LeadMother.XptoLLC();
+        var request = new RemoveLeadCommandRequest
+        {
+            Id = lead.Id,
+            Revision = lead.RowVersion
+        };
+
+        _leadRepository.GetByIdAsync(Arg.Any<Guid>(), Arg.Any<CancellationToken>())
+                       .Returns(lead);
+
+        _leadRepository.RemoveAsync(lead, lead.RowVersion, Arg.Any<CancellationToken>())
+                       .ReturnsForAnyArgs(
+                            /*On first execution, return*/ Task.CompletedTask,
+                            /*On second execution, return*/ Task.FromException<DbUpdateConcurrencyException>(new DbUpdateConcurrencyException())
+                       );
+
+        //Act
+        var firstRequest = await _handler.Handle(request, _cts.Token);
+        var secondRequest = await _handler.Handle(request, _cts.Token);
+
+        //Assert
+        firstRequest.Success.Should().BeTrue();
+        secondRequest.Success.Should().BeFalse();
+        secondRequest.Exception.Should().NotBeNull();
+        secondRequest.Exception!.ExceptionType.Should().BeEquivalentTo(typeof(DbUpdateConcurrencyException).FullName);
+        secondRequest.OperationCode.Should().Be(OperationCodes.ConcurrencyIssue);
+        await _leadRepository.Received(2).RemoveAsync(Arg.Any<Lead>(), Arg.Any<byte[]>(), Arg.Any<CancellationToken>());
+        _eventDispatcher.Received(1).AddEvent(Arg.Any<IEvent>());
     }
 
     [Fact]
--- tests/unit-tests/Application.Tests/Prospecting/Leads/Commands/UpdateLead/UpdateLeadCommandRequestHandlerTests.cs
+++ tests/unit-tests/Application.Tests/Prospecting/Leads/Commands/UpdateLead/UpdateLeadCommandRequestHandlerTests.cs
@@ -3,6 +3,7 @@ using Application.Prospecting.Leads.Commands.UpdateLead;
 using Domain.Prospecting.Entities;
 using FluentAssertions;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using NSubstitute;
 using Shared.Events;
 using Shared.Events.EventDispatching;
@@ -28,6 +29,36 @@ public sealed class UpdateLeadCommandRequestHandlerTests
         _eventDispatcher = Substitute.For<IEventDispatching>();
         _handler = new UpdateLeadCommandRequestHandler(_mediator, _eventDispatcher, _leadRepository);
         _cts = new();
+    }
+
+    [Fact]
+    public async Task Handle_ConcurrentUpdateRequestsOverSameRecord_ShouldSignalConcurrencyIssue()
+    {
+        //Arrange
+        var lead = LeadMother.XptoLLC();
+        var request = UpdateLeadCommandRequestMother.XptoIncLeadRequest();
+
+        _leadRepository.GetByIdAsync(Arg.Any<Guid>(), Arg.Any<CancellationToken>())
+                       .Returns(lead);
+
+        _leadRepository.UpdateAsync(lead, lead.RowVersion, Arg.Any<CancellationToken>())
+                       .ReturnsForAnyArgs(
+                            /*On first execution, return*/ Task.CompletedTask,
+                            /*On second execution, return*/ Task.FromException<DbUpdateConcurrencyException>(new DbUpdateConcurrencyException())
+                       );
+
+        //Act
+        var firstRequest = await _handler.Handle(request, _cts.Token);
+        var secondRequest = await _handler.Handle(request, _cts.Token);
+
+        //Assert
+        firstRequest.Success.Should().BeTrue();
+        secondRequest.Success.Should().BeFalse();
+        secondRequest.Exception.Should().NotBeNull();
+        secondRequest.Exception!.ExceptionType.Should().BeEquivalentTo(typeof(DbUpdateConcurrencyException).FullName);
+        secondRequest.OperationCode.Should().Be(OperationCodes.ConcurrencyIssue);
+        await _leadRepository.Received(2).UpdateAsync(Arg.Any<Lead>(), Arg.Any<byte[]>(), Arg.Any<CancellationToken>());
+        _eventDispatcher.Received(1).AddEvent(Arg.Any<IEvent>());
     }
 
     [Fact]
