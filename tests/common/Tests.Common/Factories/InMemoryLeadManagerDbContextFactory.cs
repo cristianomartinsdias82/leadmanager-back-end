3f9ae644b2183ef641ac071660eea7ffab26106b
cristianomartinsdias82 <crisdi@gmail.com>
2024-06-07 23:03:27 -0300

    Phase 1: upgrade all solution projects to .NET 8

--- tests/common/Tests.Common/Factories/InMemoryLeadManagerDbContextFactory.cs
+++ tests/common/Tests.Common/Factories/InMemoryLeadManagerDbContextFactory.cs
@@ -1,18 +1,18 @@
-﻿using Application.Core.Contracts.Persistence;
-using CrossCutting.Security.IAM;
-using Infrastructure.Persistence;
-using Microsoft.EntityFrameworkCore;
+﻿//using Application.Core.Contracts.Persistence;
+//using CrossCutting.Security.IAM;
+//using Infrastructure.Persistence;
+//using Microsoft.EntityFrameworkCore;
 
-namespace Tests.Common.Factories;
+//namespace Tests.Common.Factories;
 
-public static class InMemoryLeadManagerDbContextFactory
-{
-    public static ILeadManagerDbContext Create(IUserService userUservice, string? databaseName = default)
-    {
-        var options = new DbContextOptionsBuilder<LeadManagerDbContext>()
-                        .UseInMemoryDatabase(databaseName ?? Guid.NewGuid().ToString())
-                        .Options;
+//public static class InMemoryLeadManagerDbContextFactory
+//{
+//    public static ILeadManagerDbContext Create(IUserService userUservice, string? databaseName = default)
+//    {
+//        var options = new DbContextOptionsBuilder<LeadManagerDbContext>()
+//                        .UseInMemoryDatabase(databaseName ?? Guid.NewGuid().ToString())
+//                        .Options;
 
-        return new LeadManagerDbContext(options, userUservice);
-    }
-}
+//        return new LeadManagerDbContext(options, userUservice);
+//    }
+//}
